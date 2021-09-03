297649c0f2b6709a5d1d87c0a4dba2e688803358
rachelohu <rachelohu5@gmail.com>
2021-09-02 21:38:48 -0400

    first add

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let gifData;
+
+const $title = $('#title');
+const $images = $('#images');
+const $url = $('#url');
+const $rating = $('#rating');
+const $input = $('input[type = "text"]');
+
+function handleGetData(event){
+    event.preventDefault();
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${$input.val()}&api_key=A5N4Nvuh6fNQJHuYOd33Lxcr2SN3neZq&limit=5`
+    }).then(function(data){
+        console.log(data);
+        gifData = data;
+        render();
+    }, 
+    function(error){
+        console.log('Error:', error);
+    })
+} 
+
+function render(){
+    $title.text(gifData.data[0].title);
+    $images.attr("src", gifData.data[0].images.original.url);
+    $url.text(gifData.data[0].url);
+    $rating.text(gifData.data[0].rating);
+}
+
+$('form').on('submit', handleGetData);
